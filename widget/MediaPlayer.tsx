c02daa456fb56dc5ead244c3eae0d5e1293dddac
ll23002 <ll23002@ues.edu.sv>
2026-01-28 15:50:48 -0600

    Implementando imagen en MediaPlayer.tsx

--- widget/MediaPlayer.tsx
+++ widget/MediaPlayer.tsx
@@ -1,31 +1,114 @@
-import { Astal, Gtk } from "ags/gtk4";
-import { createBinding, With } from "ags";
+
+//@ts-ignore
+import Gtk from "gi://Gtk?version=4.0";
+import { execAsync } from "ags/process";
 //@ts-ignore
 import Mpris from "gi://AstalMpris";
 //@ts-ignore
+import GLib from "gi://GLib";
+//@ts-ignore
+import Gio from "gi://Gio";
+import { createBinding, With } from "ags"
+//@ts-ignore
 import Pango from "gi://Pango";
 
+function CoverArt({ player }: { player: Mpris.Player }) {
+    return (
+        <box
+            class="cover-art-area"
+            widthRequest={64}
+            heightRequest={64}
+            valign={Gtk.Align.CENTER}
+            halign={Gtk.Align.CENTER}
+        >
+            <Gtk.Picture
+                hexpand
+                vexpand
+                contentFit={Gtk.ContentFit.COVER}
+                onRealize={(self) => {
+                    const update = () => {
+                        const url = player.coverArt;
+
+                        if (!url) {
+                            self.file = null;
+                            return;
+                        }
+
+                        const cacheDir = GLib.get_user_cache_dir() + "/ags/media-covers";
+                        const safeName = GLib.base64_encode(url).replace(/[\/+=]/g, "");
+                        const destPath = `${cacheDir}/${safeName}.jpg`;
+
+                        const setImage = (path: string) => {
+                            if (GLib.file_test(path, GLib.FileTest.EXISTS)) {
+                                console.log(`[DEBUG] Intentando setear archivo: ${path}`);
+                                self.file = Gio.File.new_for_path(path);
+                            } else {
+                                console.error(`[ERROR] Archivo fantasma: ${path}`);
+                            }
+                        };
+
+                        // 1. Check Caché
+                        if (GLib.file_test(destPath, GLib.FileTest.EXISTS)) {
+                            setImage(destPath);
+                            return;
+                        }
+
+                        // 2. Setup Directorio
+                        GLib.mkdir_with_parents(cacheDir, 0o755);
+
+                        // 3. Obtener imagen
+                        let srcPath: string | null = null;
+                        if (url.startsWith("file://")) srcPath = url.substring(7);
+                        else if (url.startsWith("/")) srcPath = url;
+
+                        if (srcPath) {
+                            if (GLib.file_test(srcPath, GLib.FileTest.EXISTS)) {
+                                execAsync(["cp", srcPath, destPath])
+                                    .then(() => { if (self && self.visible) setImage(destPath); })
+                                    .catch(err => console.error("CP Error:", err));
+                            }
+                        } else if (url.startsWith("http")) {
+                            execAsync(["curl", "-s", "-o", destPath, url])
+                                .then(() => { if (self && self.visible) setImage(destPath); })
+                                .catch(err => console.error("Curl Error:", err));
+                        }
+                    };
+
+                    update();
+                    const id = player.connect("notify::cover-art", update);
+                    //@ts-ignore
+                    self._signalId = id;
+                }}
+                onDestroy={(self) => {
+                    //@ts-ignore
+                    if (self._signalId) player.disconnect(self._signalId);
+                }}
+            />
+        </box>
+    );
+}
+
+
+
+//
 function Visualizer({ player }: { player: Mpris.Player }) {
     const status = createBinding(player, "playbackStatus");
-
-return (
-    <box class="visualizer-box" valign={Gtk.Align.CENTER}>
-        {[1.2, 0.8, 1.1, 0.9, 1.3, 0.7, 0.3, 0.2, 0.1, 0.2].map((delay) => (
-            <box valign={Gtk.Align.CENTER} vexpand={false}>
-                <label
-                    class={status((s) =>
-                        s === Mpris.PlaybackStatus.PLAYING
-                            ? "visualizer-bar playing"
-                            : "visualizer-bar"
-                    )}
-                    css={`animation-duration: ${delay}s;`}
-                    valign={Gtk.Align.CENTER}
-                    vexpand={false}
-                />
-            </box>
-        ))}
-    </box>
-);
+    return (
+        <box class="visualizer-box" valign={Gtk.Align.CENTER}>
+            {[1.2, 0.8, 1.1, 0.9, 1.3, 0.7, 0.3, 0.2, 0.1, 0.2].map((delay) => (
+                <box valign={Gtk.Align.CENTER} vexpand={false}>
+                    <label
+                        class={status((s) =>
+                            s === Mpris.PlaybackStatus.PLAYING ? "visualizer-bar playing" : "visualizer-bar"
+                        )}
+                        css={`animation-duration: ${delay}s;`}
+                        valign={Gtk.Align.CENTER}
+                        vexpand={false}
+                    />
+                </box>
+            ))}
+        </box>
+    );
 }
 
 function Player({ player }: { player: Mpris.Player }) {
@@ -37,14 +120,7 @@ function Player({ player }: { player: Mpris.Player }) {
 
     return (
         <box class="media-player" spacing={12}>
-            <box
-                class="cover-art-area"
-                widthRequest={64}
-                heightRequest={64}
-                valign={Gtk.Align.CENTER}
-                halign={Gtk.Align.CENTER}
-            >
-            </box>
+            <CoverArt player={player} />
 
             <box orientation={Gtk.Orientation.VERTICAL} valign={Gtk.Align.CENTER} hexpand>
                 <label
@@ -64,36 +140,20 @@ function Player({ player }: { player: Mpris.Player }) {
                 />
 
                 <box class="controls" spacing={10} marginTop={8}>
-                    <button
-                        onClicked={() => player.previous()}
-                        visible={canGoPrev((c) => c)}
-                        class="ctrl-btn"
-                    >
+                    <button onClicked={() => player.previous()} visible={canGoPrev((c) => c)} class="ctrl-btn">
                         <label label={"\u{f048}"} />
                     </button>
 
-                    <button
-                        onClicked={() => player.play_pause()}
-                        class="play-btn"
-                    >
+                    <button onClicked={() => player.play_pause()} class="play-btn">
                         <label
-                            label={status((s) =>
-                                s === Mpris.PlaybackStatus.PLAYING
-                                    ? "\u{f04c}"
-                                    : "\u{f04b}"
-                            )}
+                            label={status((s) => (s === Mpris.PlaybackStatus.PLAYING ? "\u{f04c}" : "\u{f04b}"))}
                             css="font-size: 18px;"
                         />
                     </button>
 
                     <Visualizer player={player} />
 
-
-                    <button
-                        onClicked={() => player.next()}
-                        visible={canGoNext((c) => c)}
-                        class="ctrl-btn"
-                    >
+                    <button onClicked={() => player.next()} visible={canGoNext((c) => c)} class="ctrl-btn">
                         <label label={"\u{f051}"} />
                     </button>
                 </box>
@@ -107,10 +167,7 @@ export default function MediaPlayer() {
     const players = createBinding(mpris, "players");
 
     return (
-        <box
-            class="media-player-container"
-            visible={players((p) => p.length > 0)}
-        >
+        <box class="media-player-container" visible={players((p) => p.length > 0)}>
             <With value={players}>
                 {(ps) => {
                     //@ts-ignore

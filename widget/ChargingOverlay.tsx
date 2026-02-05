7514977db4995155c37b88ecbe23ccd25d14964c
ll23002 <ll23002@ues.edu.sv>
2026-02-05 16:01:11 -0600

    Última versión estable

--- widget/ChargingOverlay.tsx
+++ widget/ChargingOverlay.tsx
@@ -46,7 +46,7 @@ export default function ChargingOverlay() {
         layer={Astal.Layer.OVERLAY}
         exclusivity={Astal.Exclusivity.IGNORE}
         keymode={Astal.Keymode.NONE}
-        visible={false}
+        visible={true}
         anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.BOTTOM | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT}
         application={app}>
         <box
@@ -66,18 +66,38 @@ export default function ChargingOverlay() {
         </box>
     </window> as Astal.Window;
 
+    // Mostrar overlay automáticamente al iniciar (modo prueba)
+    GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, () => {
+        win.visible = true;
+        overlayState.visible = true;
+        console.log("⚡ Overlay visible - modo prueba");
+
+        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 5000, () => {
+            overlayState.visible = false;
+            return GLib.SOURCE_REMOVE;
+        });
+
+        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 5500, () => {
+            win.visible = false;
+            console.log("✓ Overlay oculto - fin de prueba");
+            return GLib.SOURCE_REMOVE;
+        });
+
+        return GLib.SOURCE_REMOVE;
+    });
+
     bat.connect("notify::charging", () => {
         if (bat.charging) {
             win.visible = true;
             overlayState.visible = true;
             console.log("⚡ Overlay visible - cargando batería");
 
-            GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, () => {
+            GLib.timeout_add(GLib.PRIORITY_DEFAULT, 800, () => {
                 overlayState.visible = false;
                 return GLib.SOURCE_REMOVE;
             });
 
-            GLib.timeout_add(GLib.PRIORITY_DEFAULT, 700, () => {
+            GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
                 win.visible = false;
                 console.log("✓ Overlay oculto - ventana cerrada");
                 return GLib.SOURCE_REMOVE;

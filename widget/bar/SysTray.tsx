cb51a465f0e94ad16e5306b2e929936975993349
ll23002 <ll23002@ues.edu.sv>
2026-02-22 14:52:53 -0600

    Code Review

--- widget/bar/SysTray.tsx
+++ widget/bar/SysTray.tsx
@@ -9,7 +9,6 @@ export function SysTray() {
         const box = <box class="systray" spacing={8} /> as Gtk.Box;
 
         const handleItemsChanged = () => {
-            // Clear current children
             let child = box.get_first_child();
             while (child) {
                 const next = child.get_next_sibling();
@@ -17,7 +16,7 @@ export function SysTray() {
                 child = next;
             }
 
-            // Add new children
+
             const items = tray.get_items();
             items.forEach((item: any) => {
                 const btn = new Gtk.MenuButton();
@@ -25,48 +24,42 @@ export function SysTray() {
                 btn.set_css_classes(["flat", "systray-item"]);
                 btn.set_has_frame(false);
 
-                // Insert action group and menu model
+
                 btn.insert_action_group("dbusmenu", item.action_group);
                 if (item.menu_model) {
                     btn.set_menu_model(item.menu_model);
                 }
 
-                // Open menu on right-click instead of default left-click
                 btn.set_primary(false);
 
-                // Create and add icon
                 const icon = new Gtk.Image();
                 icon.set_from_gicon(item.gicon);
                 icon.set_icon_size(Gtk.IconSize.NORMAL);
                 btn.set_child(icon);
 
-                // Handle left clicks to activate item
                 const click = new Gtk.GestureClick();
-                click.set_button(1); // Left-click only
+                click.set_button(1);
                 click.connect("pressed", (gesture: any, n_press: number, x: number, y: number) => {
                     item.activate(x, y);
                 });
                 btn.add_controller(click);
 
-                // Tell the app the menu is about to show on right-click
+
                 const rightClick = new Gtk.GestureClick();
-                rightClick.set_button(3); // Right-click only
+                rightClick.set_button(3);
                 rightClick.connect("pressed", () => {
                     item.about_to_show();
                 });
                 btn.add_controller(rightClick);
 
-                // Update tooltip when it changes
                 item.connect("notify::tooltip-markup", () => {
                     btn.set_tooltip_markup(item.tooltipMarkup);
                 });
 
-                // Update icon when it changes
                 item.connect("notify::gicon", () => {
                     icon.set_from_gicon(item.gicon);
                 });
-                
-                // Update menu model when it changes
+
                 item.connect("notify::menu-model", () => {
                     btn.set_menu_model(item.menu_model);
                 });
